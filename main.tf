4933bcf56805ea7e62baed18259b476f3a03f734
Harshada Ghadwaje <harshada@Harshadas-MacBook-Air.local>
2026-06-21 01:41:01 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,137 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "project-web-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+apt update -y
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+
+echo "<h1>AWS Cloud & Terraform Project
+Successfully Deployed on AWS EC2
+Infrastructure as Code (IaC) using Terraform
+EC2 | VPC | IAM | S3 | CloudWatch
+CloudWatch Monitoring & Alerting Configured</h1>" > /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "terraform-project-demo-2026-harshada"
+}
+
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_readonly" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
+  alarm_name          = "high-cpu-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions = {
+    InstanceId = aws_instance.my_ec2.id
+  }
+
+  alarm_description = "Alarm when CPU exceeds 80%"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "instance_name" {
+  default = "Terraform-EC2"
+}
+
+variable "bucket_name" {
+  default = "terraform-project-demo-2026-harshada"
+}
